723645451d99769d98d7e68f7f8349fb8566fe2c
hychoe0 <hychoe@umich.edu>
2023-01-07 18:08:50 -0500

    first commit

---
+++ stats.cpp
@@ -0,0 +1,59 @@
+// stats.cpp
+// Project UID 5366c7e2b77742d5b2142097e51561a5
+#include "stats.h"
+#include <cassert>
+#include <vector>
+#include <cmath>
+
+
+using namespace std;
+
+
+vector<pair<double, int> > summarize(vector<double> v) {
+  assert(false);
+}
+
+
+int count(vector<double> v) {
+  assert(false);
+}
+
+
+double sum(vector<double> v) {
+  assert(false);
+}
+
+
+double mean(vector<double> v) {
+  assert(false);
+}
+
+
+double median(vector<double> v) {
+  assert(false);
+}
+
+
+double mode(vector<double> v) {
+  assert(false);
+}
+
+
+double min(vector<double> v) {
+  assert(false);
+}
+
+
+double max(vector<double> v) {
+  assert(false);
+}
+
+
+double stdev(vector<double> v) {
+  assert(false);
+}
+
+
+double percentile(vector<double> v, double p) {
+  assert(false);
+}
